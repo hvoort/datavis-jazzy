1c3acfb8bfb79fe8a3e763b994eef341d44ac90a
Pieter Hameete <phameete@gmail.com>
2014-02-05 14:35:06 +0100

    Changed nesting and allowed year filters

--- src/js/usmapvis.js
+++ src/js/usmapvis.js
@@ -44,9 +44,6 @@ var usmapvis = usmapvis || (function ($, d3, undefined) {
                 
                 data_dfd = $.Deferred();
             
-            console.log(target);
-            console.log(svg);
-            
             var initialize = function () {
                     // get us state names and codes
                     d3.tsv("data/us-state-names.tsv", function(error, names) {
@@ -62,16 +59,13 @@ var usmapvis = usmapvis || (function ($, d3, undefined) {
                         // groups all data by state
                         var groups = d3.nest()
                             .key(function(d) {return d.fips_state})
-                            .entries(data),
-                            emptygroup = -1;
+                            .key(function(d) {return d.year})
+                            .entries(data);
                         
                         // Set groupname
                         groups.forEach(function (group, i) {
-                            if (!group.key || group.key === "") return emptygroup = i;
                             group.name = id_name_map[short_name_id_map[group.key.toUpperCase()]].name;
                         });
-                        // Remove non-state data group
-                        groups.splice(emptygroup, 1);
                         
                         data_dfd.resolve(groups);
                     });
@@ -117,9 +111,11 @@ var usmapvis = usmapvis || (function ($, d3, undefined) {
                     if (visualisations[key]) return console.warn("visualisation exists"); 
                 
                     data_dfd.done(function (groups) {
-                        groups.forEach(function(group) {
-                            group.stats = group.stats || {};
-                            group.stats[key] = statFunc(group);
+                        groups.forEach( function(state) {
+                            state.values.forEach(function(year) {
+                                year.stats = year.stats || {};
+                                year.stats[key] = statFunc(year);
+                            });
                         });
                     });
                 
@@ -128,7 +124,7 @@ var usmapvis = usmapvis || (function ($, d3, undefined) {
                     };
                 },
                 
-                showStatistic = function (keys, toStringFunc) {
+                showStatistic = function (keys, years, toStringFunc) {
                     if (typeof toStringFunc !== "function") return console.error("no toString function given for tooltip");
                     keys.forEach(function (key) {
                         if (!visualisations[key]) return console.error("unknown visualization " + key);
@@ -136,56 +132,61 @@ var usmapvis = usmapvis || (function ($, d3, undefined) {
                 
                     data_dfd.done(function (groups) {
                         
-                        groups.forEach(function(group) {
-                            var sum = 0;
-                            keys.forEach(function (key) {
-                                sum += group.stats[key];
-                            });
-                            
-                            group.mergedstats = sum/keys.length;
+                    // aggregates the percentages for the selected types of cultural activities and selected years
+                    groups.forEach(function(group) {
+                        var sum = 0;
+                        group.values.forEach(function(year) {
+                            if(years.indexOf(year.key) != -1) {
+                                keys.forEach(function (key) {
+                                    sum += year.stats[key];
+                                });
+                                sum = sum/keys.length;
+                            }
+                        });
+                        group.mergedstats = sum/years.length;
+                    });
+                    
+                    // compute maximum percentage over all states and set a gradient color range
+                    var min = d3.min(groups, function(group) { return +group.mergedstats; }), 
+                        max = d3.max(groups, function(group) { return +group.mergedstats; }),
+                        colorScale = d3.scale.linear()
+                            .range(['white', 'darkred'])
+                            .domain([min, max]);
+                    
+                   
+                    groups.forEach(function(group) {
+                        var $state = $("#"+group.key.toUpperCase());
+                        
+                        // unbind all previous events
+                        $state.unbind();
+                        
+                        // color the state according to the stats
+                        $state.css("fill", colorScale(group.mergedstats));
+                        
+                        // shows tooltip on mouseover
+                        $state.on("mouseover", function() {
+                            var statecode = $(this).attr("id"),
+                                state = $(this).attr("state"),
+                                centroid = centroids[statecode],
+                                x = centroid[0]; // + target.position().left;
+                                y = centroid[1]; //+ target.position().top;
+             
+                            tooltip.html(toStringFunc(group, group.mergedstats))
+                                .style("left", x + "px")
+                                .style("top", y - 20 + "px");
+                            tooltip.transition().duration(200).style("opacity", 1);
+                        }).on("mouseout", function(d) {
+                            tooltip.transition().duration(500).style("opacity", 0);
                         });
                         
-                        // compute maximum percentage over all states and set a gradient color range
-                        var min = d3.min(groups, function(group) { return +group.mergedstats; }), 
-                            max = d3.max(groups, function(group) { return +group.mergedstats; }),
-                            colorScale = d3.scale.linear()
-                                .range(['white', 'darkred'])
-                                .domain([min, max]);
+                        // State interaction with other graphs
+                        $state.on("click", (function (s) {
+                            return function(e) {
+                                console.log("clicked on", s.key);
+                            }
+                        }(group)));
                         
-                       
-                        groups.forEach(function(group) {
-                            var $state = $("#"+group.key.toUpperCase());
-                            
-                            // unbind all previous events
-                            $state.unbind();
-                            
-                            // color the state according to the stats
-                            $state.css("fill", colorScale(group.mergedstats));
-                            
-                            // shows tooltip on mouseover
-                            $state.on("mouseover", function() {
-                                var statecode = $(this).attr("id"),
-                                    state = $(this).attr("state"),
-                                    centroid = centroids[statecode],
-                                    x = centroid[0]; // + target.position().left;
-                                    y = centroid[1]; //+ target.position().top;
-                 
-                                tooltip.html(toStringFunc(group, group.mergedstats))
-                                    .style("left", x + "px")
-                                    .style("top", y - 20 + "px");
-                                tooltip.transition().duration(200).style("opacity", 1);
-                            }).on("mouseout", function(d) {
-                                tooltip.transition().duration(500).style("opacity", 0);
-                            });
-                            
-                            // State interaction with other graphs
-                            $state.on("click", (function (s) {
-                                return function(e) {
-                                    console.log("clicked on", s.key);
-                                }
-                            }(group)));
-                            
-                        });
+                    });
                     });                    
                 };
             
